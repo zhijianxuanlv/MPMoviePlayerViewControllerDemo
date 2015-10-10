bf009f36282512ac6bb536238d2d68ab18c828d9
luocheng <1059326760@qq.com>
2015-10-10 14:32:02 +0800

    Initial Commit

---
+++ VideoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoDemo
+//
+//  Created by 罗成 on 15/10/10.
+//  Copyright (c) 2015年 罗成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VideoDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoDemo
+//
+//  Created by 罗成 on 15/10/10.
+//  Copyright (c) 2015年 罗成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
